90b845c38e4f7f58660fa403176eddc3d94eb068
Hendry <hendryg56@gmail.com>
2021-05-10 19:13:46 +0700

    changes made

---
+++ app/src/main/java/com/sendo/safeter/TopUpPage.java
@@ -0,0 +1,14 @@
+package com.sendo.safeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up_page);
+    }
+}
