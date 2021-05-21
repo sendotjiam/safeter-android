ddd1da7d58e833d3aa6716ae53b00ecff3f85093
Hendry <hendryg56@gmail.com>
2021-05-21 17:04:15 +0700

    backend

--- app/src/main/java/com/sendo/safeter/ProfilePage.java
+++ app/src/main/java/com/sendo/safeter/ProfilePage.java
@@ -7,9 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sendo.safeter.database.ContactDB;
+import com.sendo.safeter.database.UserDB;
+import com.sendo.safeter.models.Contact;
+import com.sendo.safeter.models.User;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -20,19 +27,25 @@ public class ProfilePage extends AppCompatActivity {
     Button btn_topup;
     TextView prof_name, prof_address, prof_email, prof_number;
     Spinner spin_emergency;
-    TextView cont_name, cont_number;
+    EditText cont_name, cont_number;
     ImageButton add_contact;
     ArrayList<EmergencyContact> listcontact;
+    ArrayList<Contact> contacts = new ArrayList<>();
+    int userid;
+    int tampung;
+    UserDB userDB;
+    ContactDB contactDB;
+    User user = new User();
+    Contact contact = new Contact();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
-        getSupportActionBar().hide();
 
         balance = findViewById(R.id.balance);
         btn_topup = findViewById(R.id.btn_topup);
-        prof_name = findViewById(R.id.name);
+        prof_name = findViewById(R.id.prof_name);
         prof_address = findViewById(R.id.prof_address);
         prof_email = findViewById(R.id.prof_email);
         prof_number = findViewById(R.id.prof_number);
@@ -41,27 +54,78 @@ public class ProfilePage extends AppCompatActivity {
         cont_number = findViewById(R.id.cont_number);
         add_contact = findViewById(R.id.add_contact);
 
-        listcontact = new ArrayList<>();
-        EmergencyContact contact = new EmergencyContact("Daddy", "081261234567");
-        listcontact.add(contact);
-        EmergencyContact contact1 = new EmergencyContact("Mommy", "081261234567");
-        listcontact.add(contact1);
+        userDB = new UserDB(this);
+        contactDB = new ContactDB(this);
+
+//        listcontact = new ArrayList<>();
+//        EmergencyContact contact = new EmergencyContact("Daddy", "081261234567");
+//        listcontact.add(contact);
+//        EmergencyContact contact1 = new EmergencyContact("Mommy", "081261234567");
+//        listcontact.add(contact1);
+
+        tampung = contactDB.countTableSize();
+        if(tampung != 0){
+            for(int i = 1; i <= tampung; i++){
+                contact = contactDB.getContact1(i);
+                if(contact.getUser_id() == userid){
+                    EmergencyContact contact2 = new EmergencyContact(contact.getEmergency_contact_name(), String.valueOf(contact.getEmergency_contact_phonenumber()));
+                    listcontact.add(contact2);
+                }
+            }
+        }
+
+
+//        Toast.makeText(this, contacts.get(0).getEmergency_contact_name(), Toast.LENGTH_SHORT).show();
+//        if(contacts != null){
+//            for(int i = 0; i <= contacts.size(); i++){
+////                Toast.makeText(this, contacts.get(i).getEmergency_contact_name(), Toast.LENGTH_SHORT).show();
+//                EmergencyContact contact2 = new EmergencyContact(contacts.get(i).getEmergency_contact_name(), String.valueOf(contacts.get(i).getEmergency_contact_phonenumber()));
+//                listcontact.add(contact2);
+//            }
+//        }
 
         ArrayAdapter<EmergencyContact> adapter = new ArrayAdapter<EmergencyContact>(this,
                 R.layout.spinner, listcontact);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin_emergency.setAdapter(adapter);
 
+        Intent intent = getIntent();
+        userid = intent.getIntExtra("USER_ID_PROFILE", 0);
+
+        user = userDB.getUser(userid);
+
+        prof_name.setText(user.getName());
+        prof_address.setText(user.getAddress());
+        prof_email.setText(user.getEmail());
+        prof_number.setText(user.getPhoneNumber());
+        balance.setText("Rp." + user.getBalance() + ",00");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        user = userDB.getUser(userid);
+
+        prof_name.setText(user.getName());
+        prof_address.setText(user.getAddress());
+        prof_email.setText(user.getEmail());
+        prof_number.setText(user.getPhoneNumber());
+        balance.setText("Rp." + user.getBalance() + ",00");
     }
 
     public void subscription(View view) {
         Intent intent = new Intent(this, SubscriptionPage.class);
+        intent.putExtra("USER_ID_PROFILE", userid);
         startActivity(intent);
+        finish();
     }
 
     public void call(View view) {
         Intent intent = new Intent(this, HomePage.class);
+        intent.putExtra("USER_ID_PROFILE", userid);
         startActivity(intent);
+        finish();
     }
 
     public void profile(View view) {
@@ -70,11 +134,17 @@ public class ProfilePage extends AppCompatActivity {
 
     public void topup(View view) {
         Intent intent = new Intent(this, TopUpPage.class);
+        intent.putExtra("USER_ID_TOPUP", userid);
         startActivity(intent);
     }
 
     public void addcontact(View view) {
+        Contact contact = new Contact();
+        contact.setUser_id(userid);
+        contact.setEmergency_contact_name(cont_name.getText().toString());
+        contact.setEmergency_contact_phonenumber(Long.parseLong(cont_number.getText().toString()));
 
+        contactDB.insertContact(contact);
     }
 
 }
